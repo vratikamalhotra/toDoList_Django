7d44c68fee3a12381f2cfedb6addd13831c1349a
Vratika <malhotravratika12@gmail.com>
2020-12-22 13:53:44 +0530

    initial commit of to-do django project

---
+++ home/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model): 
+    taskTitle = models.CharField(max_length=30)
+    taskDescription = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.taskTitle
+    
+
+     
---
+++ home/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, HttpResponse
+from home.models import Task
+# Create your views here.
+def home(request):
+    context = {'success': False}
+    
+    if request.method == "POST":
+        #handle the form
+        title = request.POST['title']
+        desc = request.POST['desc']
+        ins=Task(taskTitle=title, taskDescription=desc)
+        ins.save()
+        context = {'success': True}
+    return render(request, 'index.html', context)
+
+def tasks(request):
+    allTasks = Task.objects.all() #fetch data from table
+    context = {'tasks': allTasks}
+    return render(request, 'tasks.html',context)
+
+
+
+
+
+
